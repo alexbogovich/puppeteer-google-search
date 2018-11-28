aee895632359fa2421f16c6b868ff923a6223df5
Alex Bogovich <bogovich.alexander@gmail.com>
2018-11-28 14:58:05 +0700

    add translate

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,10 @@ import url from 'url'
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/image', async (req, res) => {
-  const {search, https = false, hl = 'en'} = req.query
+  const { search, https = false, hl = 'en' } = req.query
   if (!search) {
     return res.send('Please provide URL as GET image list')
   }
@@ -35,7 +37,7 @@ app.get('/image', async (req, res) => {
 })
 
 app.get('/title', async (req, res) => {
-  const {search, hl = 'en'} = req.query
+  const { search, hl = 'en' } = req.query
 
   if (!search) {
     return res.send('Please provide URL as GET image list')
@@ -56,6 +58,44 @@ app.get('/title', async (req, res) => {
   browser.close()
 
   res.send(JSON.stringify({ title: title }))
+})
+
+app.post('/translate', async (req, res) => {
+  const { from = 'ru', to = 'en' } = req.query
+  const { search } = req.body
+
+  if (!search) {
+    return res.send('Please provide URL as GET image list')
+  }
+
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox'],
+  })
+  const page = await browser.newPage()
+  const searchQuery = `https://translate.google.com/?um=1&ie=UTF-8&hl=en&client=tw-ob#${from}/${to}/${encodeURIComponent(search)}`
+  console.log(`navigate to ${searchQuery}`)
+  await page.goto(searchQuery)
+  const text = await page.evaluate(() => {
+    return new Promise((resolve, reject) => {
+      const getTranslate = () => document.querySelectorAll('span#result_box')[0]
+      let spent = 0;
+      const timer = setInterval(() => {
+        const textContent = getTranslate().textContent
+        if (textContent && textContent !== '') {
+          clearInterval(timer)
+          resolve(textContent)
+        } else if (spent > 3000) {
+          clearInterval(timer)
+          reject("spent more than 3 sec")
+        }
+        spent += 100
+      }, 100)
+    })
+
+  })
+  browser.close()
+
+  res.send(JSON.stringify({ translate: text, source: search }))
 })
 
 const server = app.listen(process.env.PORT || 8080, (err: any) => {
